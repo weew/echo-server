0c59313db873d61278b34f941b1703764f456d0a
Maxim Kott <maximkott@gmail.com>
2016-05-18 10:15:39 +0200

    Upgrading to the latest php-http version.

--- src/Weew/EchoServer/EchoServer.php
+++ src/Weew/EchoServer/EchoServer.php
@@ -7,10 +7,23 @@ use Weew\Http\Responses\JsonResponse;
 
 class EchoServer {
     /**
+     * @var string
+     */
+    protected $trunksPath;
+
+    /**
+     * EchoServer constructor.
+     */
+    public function __construct() {
+        $this->trunksPath = path(WEB_DIR, '/trunks');
+    }
+
+    /**
      * @param IHttpRequest $request
      */
     public function handle(IHttpRequest $request) {
         $data = $this->extractRequestData($request);
+        $this->addToTrunk($request, $data);
         $this->sendJsonResponse($data);
     }
 
@@ -19,7 +32,7 @@ class EchoServer {
      */
     protected function sendJsonResponse(array $data) {
         $response = new JsonResponse();
-        $response->setJsonContent($data, JSON_PRETTY_PRINT);
+        $response->setContent(json_encode($data, JSON_PRETTY_PRINT));
 
         $response->send();
     }
@@ -35,5 +48,28 @@ class EchoServer {
         $data['url']['full'] = $request->getUrl()->toString();
 
         return $data;
+    }
+
+    /**
+     * @param IHttpRequest $request
+     * @param array $data
+     */
+    protected function addToTrunk(IHttpRequest $request, array $data) {
+        $trunk = $request->getUrl()->getQuery()->get('trunk');
+
+        if ($trunk !== null) {
+            $trunk = str_replace(['/', '.'], '', $trunk);
+
+            if (strlen($trunk) > 0) {
+                $path =  path($this->trunksPath, $trunk . '.txt');
+
+                if ( ! file_exists($path)) {
+                    file_create($path);
+                }
+
+                file_append($path, s("\n\n%s %s\n\n", date('c'), str_repeat('=', 80)));
+                file_append($path, json_encode($data, JSON_PRETTY_PRINT));
+            }
+        }
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,6 +1,11 @@
 <?php
 
+use Weew\EchoServer\EchoServer;
+use Weew\Http\Requests\CurrentRequest;
+
+define('WEB_DIR', __DIR__);
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$server = new \Weew\EchoServer\EchoServer();
-$server->handle(new \Weew\Http\Requests\CurrentRequest());
+$server = new EchoServer();
+$server->handle(new CurrentRequest());
